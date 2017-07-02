41e55f99fb95c006f1b0009d7f573f19e6d794eb
Sandro Pasquali <sandro.pasquali@twintechs.com>
2017-07-02 12:39:24 -0400

    ++

--- october.js
+++ october.js
@@ -1,26 +1,8 @@
-module.exports = function(opts) {
+'use strict';
 
-	var facade = new function(){};
+let october = require('wikijs').default().page('october');
 
-	var day;
-	
-	for(day=0; day <= 31; day++) {
-		facade["day" + day] = (function(d) {
-			return function() {
-				this.day = d;
-				
-				return this;
-			}
-		})(day);
-	}
-	
-	facade.translate = function(toL) {
-		return ({
-			french 	: "Octobre",
-			spanish	: "Octubre",
-			german  : "Oktober"
-		})[toL] || null;
-	}
-	
-	return facade;
+module.exports = {
+	data : () => october.then(page => page.content()),
+	images : () => october.then(page => page.images())
 };
